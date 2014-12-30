7918d578166d5e5d0b617585318e7a3c2061741a
lipido <dgpena@uvigo.es>
2014-12-30 18:48:03 +0100

    Remove embedded args4j source code. Now it is a Maven dependency.

--- alter-lib/src/main/java/org/kohsuke/args4j/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Paquete principal del parseador de línea de comandos args4j.
- * 
- */
-package org.kohsuke.args4j;
--- alter-lib/src/main/java/org/kohsuke/args4j/spi/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Clases que extienden el coportamiento de args4j.
- */
-package org.kohsuke.args4j.spi;
